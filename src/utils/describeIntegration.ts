1f3c7ae38e67486579db73e649c7830cdfd355d3
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-04-07 10:34:21 +0200

    chore: skip tests

--- src/utils/describeIntegration.ts
+++ src/utils/describeIntegration.ts
@@ -1,1 +1,1 @@
-export const describeIntegration = process.env.JEST_RUN_INTEGRATION_TESTS ? describe : describe.skip;
+export const describeIntegration = process.env.JEST_RUN_INTEGRATION_TESTS == 'TRUE' ? describe : describe.skip;
